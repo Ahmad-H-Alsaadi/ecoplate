cbdb95024f136ef389bcfac588da2e6e02071228
Ahmad-H-Alsaadi <ahmad.h.alsaadi@gmail.com>
2024-07-10 23:06:29 +0300

    'refactor: creaate classes to set directions'

--- lib/app/dashboard/view/dashboard_view.dart
+++ lib/app/dashboard/view/dashboard_view.dart
@@ -10,6 +10,10 @@ class DashboardView extends StatefulWidget {
 class _DashboardViewState extends State<DashboardView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Text("DashBoard"),
+      ),
+    );
   }
 }
